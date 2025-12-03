17b017ae1d9bb0bebf8294ba61cbe61c4853cbb5
Camila Santos <camilasantosbatista2222@gmail.com>
2025-12-02 21:24:19 -0300

    Refactor Produto class to use wrapper types

--- AppMogiTech/src/model/Produto.java
+++ AppMogiTech/src/model/Produto.java
@@ -1,24 +1,44 @@
 /*
  * @author Ryan B. | Camila S.
  */
-package model;
+package modelo;
 
 public class Produto {
-    private int id;
-    private String nome;
-    private double preco;
-    private int quantidade;
 
-    // Getters e Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    private Integer id;
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Produto() {}
+
+    public Produto(Integer id, String nome, Double preco, Integer quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Produto(String nome, Double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
-    public double getPreco() { return preco; }
-    public void setPreco(double preco) { this.preco = preco; }
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
 
-    public int getQuantidade() { return quantidade; }
-    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+    public Integer getQuantidade() { return quantidade; }
+    public void setQuantidade(Integer quantidade) { this.quantidade = quantidade; }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
 }
